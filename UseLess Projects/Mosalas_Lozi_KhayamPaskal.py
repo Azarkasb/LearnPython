08deaf958e48e2d9d9819e1426c4f07ec83bd474
AZ <azarkasbma@gmail.com>
2021-07-26 12:26:11 +0430

    Okay

--- UseLess Projects/Mosalas_Lozi_KhayamPaskal.py
+++ UseLess Projects/Mosalas_Lozi_KhayamPaskal.py
@@ -1,13 +1,14 @@
+'''
+Barname baraye chap mosalas, lozi, mosalasKhayam_paskal motanaseb ba n vorodi
+'''
 #All in one
-'''
-Azarkasb
-'''
+#AZ
 n = int(input('Enter an integer : '))
 
 #1
 for i in range(n - 1, -1, -1):
     print(' ' * i + '*' * (n - i))
-# HACK: # HACK: {self.assert_(boolean expression, 'message') for value in variable}
+
 #2
 print('next'.center(120, '.'))
 np = n//2
@@ -28,11 +29,32 @@ for i in range(1, n):
     #print
     for j in l[1:-1]:
         s += str(j) + ' '
-    print(s.center(2 * n))
+    print(s.center(4 * n))
     s = ''
     #next
     for k in range(i + 1):
         x = l.pop()
         l.insert(0, (x + l[-1]))
-    else:
+    else :
         l.insert(0, 0)
+
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
