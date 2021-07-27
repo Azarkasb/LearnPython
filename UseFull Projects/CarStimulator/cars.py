6f9154f5578c2eaab97cba2d10cf2470a7c219fd
AZ <azarkasbma@gmail.com>
2021-07-27 13:24:32 +0430

    CarStimulator Added

---
+++ UseFull Projects/CarStimulator/cars.py
@@ -0,0 +1,79 @@
+
+class Car():
+    def __init__(self, color, speed):
+        self.colorList = ['orange', 'black', 'blue', 'green', 'colorful', 'yellow', 'red']
+        self.color = 'black'
+        self.color = color
+        self.speed = 0
+        self.speed = speed
+        self.start = False
+    def __repr__(self):
+        return f'I am a {type(self).__name__} {self.color} car'
+    def __str__(self):
+        return f'I am a {type(self).__name__} {self.color} car'
+    def turnOn(self):
+        self.start = True
+    def turnOff(self):
+        self.start = False
+
+    @property
+    def colorList(self):
+        return self.__colorList
+    @colorList.setter
+    def colorList(self, colorList):
+        if isinstance(colorList, list):
+            self.__colorList = colorList
+        
+    @property
+    def color(self):
+        return self.__color
+    @color.setter
+    def color(self, color):
+        if color in self.colorList:
+            self.__color = color
+
+    @property
+    def speed(self):
+        return self.__speed
+    @speed.setter
+    def speed(self, speed):
+        if (isinstance(speed, float) or isinstance(speed, int)) and speed >= 0:
+            self.__speed = speed
+
+    @property
+    def gearBox(self):
+        return self.__gearBox
+    @gearBox.setter
+    def gearBox(self, gearList):
+        if isinstance(gearList, list):
+            self.__gearBox = gearList
+
+    @property
+    def gear(self):
+        return self.__gear
+    @gear.setter
+    def gear(self, gear):
+        if gear in self.__gearBox:
+            self.__gear = gear
+
+    @property
+    def start(self):
+        return self.__start
+    @start.setter
+    def start(self, start):
+        if isinstance(start, bool):
+            self.__start = start
+    
+class Simple(Car):
+    def __init__(self, color, speed):
+        super().__init__(color, speed)
+        self.gearBox = ['0', 'R', '1', '2', '3', '4', '5']
+        self.gear = '0'
+    
+
+
+class Auto(Car):
+    def __init__(self, color, speed):
+        super().__init__(color, speed)
+        self.gearBox = ['P', 'N', 'R', 'D']
+        self.gear = 'P'
---
+++ UseFull Projects/CarStimulator/motion.py
@@ -0,0 +1,115 @@
+import turtle
+import cars
+import time
+import random
+
+#GlobalVariables
+
+#input
+
+color = input('Choose color between [orange, black, blue, green, colorful, yellow, red] : ')
+speed = float(input(f'Choose speed between 0 to 240 : '))
+if input('Choose the type of car [auto, simple] : ') == 'auto':
+    car = cars.Auto(color, speed)
+else :
+    car = cars.Simple(color, speed)
+##Process
+#Ready
+screen = turtle.getscreen()
+screen.setup(800, 600)
+screen.register_shape(f'{color}.gif')
+
+carShape = turtle.Turtle(f'{color}.gif')
+carShape.penup()
+turtle.hideturtle()
+carShape.speed = 10
+
+gear = turtle.Turtle(visible = False)
+gear.speed(0)
+gear.penup()
+gear.goto(-40, 170)
+gear.write(f'Gear : {car.gear}')
+
+writer = turtle.Turtle(visible = False)
+writer.speed(0)
+writer.penup()
+writer.goto(-40, 200)
+writer.write('Hello')
+
+
+
+for i in range(0, 5):
+    writer.clear()
+    writer.write(str(5 - i).ljust(2) + 'seconds left')
+    time.sleep(1)
+writer.clear()
+writer.write('Start')
+
+##+A
+if type(car).__name__ == 'Auto':
+    car.gear = 'D'
+else :
+    car.gear = '1'
+
+gear.clear()
+gear.write(f'Gear : {car.gear}')
+
+for i in range(30):
+    time.sleep(0.24 - (car.speed / 1000))
+    carShape.forward(10)
+
+    if type(car).__name__ == 'Simple':
+        if i == 10:
+            car.gear = '2'
+            gear.clear()
+            gear.write(f'Gear : {car.gear}')
+        elif i == 17:
+            car.gear = '3'
+            gear.clear()
+            gear.write(f'Gear : {car.gear}')
+
+###-2A
+car.gear = 'R'
+
+gear.clear()
+gear.write(f'Gear : {car.gear}')
+
+for i in range(60):
+    time.sleep(0.24 - (car.speed / 1000))
+    carShape.bk(10)
+
+##+A
+if type(car).__name__ == 'Auto':
+    car.gear = 'D'
+else :
+    car.gear = '1'
+
+gear.clear()
+gear.write(f'Gear : {car.gear}')
+
+for i in range(30):
+    time.sleep(0.24 - (car.speed / 1000))
+    carShape.forward(10)
+
+    if type(car).__name__ == 'Simple':
+        if i == 10:
+            car.gear = '2'
+            gear.clear()
+            gear.write(f'Gear : {car.gear}')
+        elif i == 17:
+            car.gear = '3'
+            gear.clear()
+            gear.write(f'Gear : {car.gear}')
+
+##End
+if type(car).__name__ == 'Auto':
+    car.gear = 'P'
+else :
+    car.gear = '0'
+
+gear.clear()
+gear.write(f'Gear : {car.gear}')
+writer.clear()
+writer.write('End')
+
+
