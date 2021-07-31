3a169d130f89933f3ee779987dc7cd3685698a63
AZ <azarkasbma@gmail.com>
2021-07-31 17:13:06 +0430

    Update test_Recursive.py name

--- UseLessProjects/Recursives/test_Recursive.py
+++ UseLessProjects/Recursives/test_Recursive.py
@@ -1,5 +1,6 @@
 import unittest
 from Recursive_main import  raddition as rsum
+from fractions import Fraction
 
 class TestRecursive(unittest.TestCase):
     def test_1(self):
@@ -8,6 +9,8 @@ class TestRecursive(unittest.TestCase):
         self.assertEqual(rsum(2, 3), 5)
     def test_3(self):
         self.assertTrue(rsum(1, 4))
-
+    def test_4(self):
+        self.assertEqual(Fraction(1, 3) + Fraction(1, 3), Fraction(1, 2))
+    
 if __name__ == '__main__':
     unittest.main()
